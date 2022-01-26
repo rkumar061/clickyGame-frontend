0cd19dab2acee631ee8c226b5c87d3f7f47117c4
Raunak-Hacker <raunak200308@gmail.com>
2022-01-26 23:58:50 +0530

    Format

--- src/components/multyplayerGame.vue
+++ src/components/multyplayerGame.vue
@@ -1,43 +1,34 @@
 <template>
-    <div class="game">
-            <div class="score">
-                <div class="scoreValue" :style="BarColor">
-
-                </div>
-            </div>
-            <div class="button">
-                <button @click="buttonClicked">
-                    <div class="buttonText">
-                        Click Me
-                    </div>
-                </button>
-            </div>
-        </div>
+  <div class="game">
+    <div class="score">
+      <div class="scoreValue" :style="BarColor"></div>
+    </div>
+    <div class="button">
+      <button @click="buttonClicked">
+        <div class="buttonText">Click Me</div>
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['scoreValue', 'time'],
-    methods: {
-        buttonClicked() {
-
-            this.$emit('button-clicked');
-            // console.log("buttonClicked");
-        }
+  props: ["scoreValue", "time"],
+  methods: {
+    buttonClicked() {
+      this.$emit("button-clicked");
+      // console.log("buttonClicked");
     },
-    
-    computed: {
- 
-        BarColor() {
-            return {
-                'width': this.scoreValue + '%'
-            }
-        },
-    },
-}
+  },
 
+  computed: {
+    BarColor() {
+      return {
+        width: this.scoreValue + "%",
+      };
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/components/nameComponent.vue
+++ src/components/nameComponent.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="container">
     <div class="instruction">
-      Enter Your name below click play button it will tack you to single player game to know your level for better match making in multiplayer game.
+      Enter Your name below click play button it will tack you to single player
+      game to know your level for better match making in multiplayer game.
     </div>
     <div class="nameip">
-    <input type="text" placeholder="Your Name" v-model="name">
-    <button @click="nameToggle()">Play</button>
+      <input type="text" placeholder="Your Name" v-model="name" />
+      <button @click="nameToggle()">Play</button>
     </div>
-    
   </div>
 </template>
 
@@ -15,83 +15,77 @@
 export default {
   data() {
     return {
-      name: '',
-    }
-  
+      name: "",
+    };
   },
   methods: {
     nameToggle() {
-      if (this.name === '') {
-        alert('Please Enter Your Name');
+      if (this.name === "") {
+        alert("Please Enter Your Name");
       } else {
-        this.$emit('name-toggle', this.name);
+        this.$emit("name-toggle", this.name);
       }
-    }
-  }
-}
-
+    },
+  },
+};
 </script>
 
 <style>
+.container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30vh;
+}
 
+.instruction {
+  font-size: 20px;
+  color: #000;
+  text-align: center;
+  max-width: 25rem;
+}
+
+.nameip {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+input {
+  width: 200px;
+  height: 40px;
+  border-radius: 5px;
+  border: 1px solid #000;
+  padding: 0 10px;
+}
+
+button {
+  width: 100px;
+  height: 40px;
+  border-radius: 5px;
+  border: 1px solid #000;
+  background-color: #000;
+  color: #fff;
+  font-size: 20px;
+  cursor: pointer;
+  margin: 1rem;
+}
+@media screen and (max-width: 600px) {
   .container {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top: 30vh;
+    flex-direction: column;
+    margin-top: 15vh;
   }
-
-  .instruction {
-    font-size: 20px;
-    color: #000;
-    text-align: center;
-    max-width: 25rem;
-  }
-
   .nameip {
-    display: flex;
-    justify-content: center;
+    margin-top: 2rem;
+    flex-direction: column;
     align-items: center;
+    width: 90%;
   }
-
+  .container button,
   input {
-    width: 200px;
-    height: 40px;
-    border-radius: 5px;
-    border: 1px solid #000;
-    padding: 0 10px;
+    width: 100%;
   }
-
-  button {
-    width: 100px;
-    height: 40px;
-    border-radius: 5px;
-    border: 1px solid #000;
-    background-color: #000;
-    color: #fff;
-    font-size: 20px;
-    cursor: pointer;
-    margin: 1rem;
-  }
-  @media screen and (max-width: 600px) {
-    .container {
-      flex-direction: column;
-      margin-top: 15vh;
-    }
-    .nameip {
-      margin-top: 2rem;
-      flex-direction: column;
-      align-items: center;
-      width: 90%;
-    }
-    .container button, input {
-      width: 100%;
-    }
-
-  }
-    
-  
-
+}
 </style>
